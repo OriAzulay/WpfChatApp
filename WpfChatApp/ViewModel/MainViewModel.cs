a075da1e58090847e6e69b51132930f3367f5ca4
KareenSalameh <itskareen3@hotmail.com>
2022-06-25 22:24:55 +0300

    Finals initial

--- WpfChatApp/ViewModel/MainViewModel.cs
+++ WpfChatApp/ViewModel/MainViewModel.cs
@@ -5,23 +5,58 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfChatApp.Core;
 
 namespace WpfChatApp.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
 
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        public RelayCommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
 
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                });
+                Message = " ";
+            });
+
             Messages.Add(new MessageModel
             {
-                Username = "Bob",
+                Username = "Alice",
                 UsernameColor = "#409aff",
                 ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
                 Message = "Test",
@@ -30,52 +65,50 @@ namespace WpfChatApp.ViewModel
                 FirstMessage = true
             });
 
-            for(int i = 0; i < 3; i++)
+
+            Messages.Add(new MessageModel
             {
-                Messages.Add(new MessageModel
-                {
-                    Username = "Alice",
-                    UsernameColor = "#409aff",
-                    ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
-                    Message = "Test",
-                    Time = DateTime.Now,
-                    IsNativeOrigin = false,
-                    FirstMessage = false
-                });
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                Messages.Add(new MessageModel
-                {
-                    Username = "Johny",
-                    UsernameColor = "#409aff",
-                    ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
-                    Message = "Test",
-                    Time = DateTime.Now,
-                    IsNativeOrigin = true,
-                    FirstMessage = false
-                });
-            }
+                Username = "Alice",
+                UsernameColor = "#409aff",
+                ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
+                Message = "Welcome here!",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = false
+            });
+
             Messages.Add(new MessageModel
             {
                 Username = "Johny",
                 UsernameColor = "#409aff",
                 ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
-                Message = "Last",
+                Message = "This is my last message",
                 Time = DateTime.Now,
                 IsNativeOrigin = true,
             });
 
-            for (int i = 0; i < 5; i++)
+            Contacts.Add(new ContactModel
             {
-                Contacts.Add(new ContactModel
-                {
-                    Username = $"Bob {i}",
-                    ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
-                    Messages = Messages
+                Username = "Bob",
+                ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
+                Messages = Messages
 
-                });
-            }
+            });
+            Contacts.Add(new ContactModel
+            {
+                Username = "Alice",
+                ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
+                Messages = Messages
+
+            });
+            Contacts.Add(new ContactModel
+            {
+                Username = "Johny",
+                ImageSource = "https://www.business2community.com/social-media-articles/importance-profile-picture-career-01899604",
+                Messages = Messages
+
+            });
+
         }
 
 
